2ffb309bbd14b9c5cf5d3401be9c056c7fc52f1d
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2024-09-08 14:50:14 -0700

    add footer and home component files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		<main>
+			<section className="hero"></section>
+			<section className="specials"></section>
+			<section className="testimonials"></section>
+			<section className="seattle"></section>
+		</main>
+	);
+};
+
+export default Home;
