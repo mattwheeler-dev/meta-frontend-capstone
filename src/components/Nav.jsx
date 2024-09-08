c5de156eceb743bf09842d5d22ee131ee189129f
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2024-09-07 22:10:25 -0700

    add logo to nav, add meta tags

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import Logo from "../assets/Logo";
+
+const Nav = () => {
+	return (
+		<nav>
+			<Logo />
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Menu</li>
+				<li>Reservations</li>
+				<li>Order Online</li>
+				<li>Login</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Nav;
