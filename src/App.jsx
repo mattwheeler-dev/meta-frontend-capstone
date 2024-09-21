705aaa96544e2074e16fb0d3f172d8f5144c8b8b
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2024-09-21 16:39:06 -0700

    move availableTimes to App.jsx. add context to outlet. small size adjustment on menu page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 function App() {
+	const [availableTimes] = useState([
+		"17:00",
+		"18:00",
+		"19:00",
+		"20:00",
+		"21:00",
+		"22:00",
+		"23:00",
+	]);
+
 	return (
 		<>
 			<Nav />
-			<Outlet />
+			<Outlet context={{ availableTimes }} />
 			<Footer />
 		</>
 	);
--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,27 +1,63 @@
+import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 import "./ReservationForm.css";
 
 const ReservationForm = () => {
+	const { availableTimes } = useOutletContext();
+	const [date, setDate] = useState("");
+	const [time, setTime] = useState("");
+	const [numGuests, setNumGuests] = useState(1);
+	const [occasion, setOccasion] = useState("");
+
+	const timeOptions = availableTimes.map((resTime) => {
+		return <option key={resTime}>{resTime}</option>;
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(date, time, numGuests, occasion);
+	};
+
 	return (
-		<form className="res-form">
+		<form className="res-form" onSubmit={handleSubmit}>
 			<label htmlFor="res-date">Choose date</label>
-			<input type="date" id="res-date" />
+			<input
+				type="date"
+				id="res-date"
+				value={date}
+				onChange={(e) => setDate(e.target.value)}
+			/>
+
 			<label htmlFor="res-time">Choose time</label>
-			<select id="res-time ">
-				<option>17:00</option>
-				<option>18:00</option>
-				<option>19:00</option>
-				<option>20:00</option>
-				<option>21:00</option>
-				<option>22:00</option>
+			<select
+				id="res-time"
+				value={time}
+				onChange={(e) => setTime(e.target.value)}
+			>
+				{timeOptions}
 			</select>
+
 			<label htmlFor="guests">Number of guests</label>
-			<input type="number" placeholder="1" min="1" max="10" id="guests" />
+			<input
+				type="number"
+				min="1"
+				max="10"
+				id="guests"
+				value={numGuests}
+				onChange={(e) => setNumGuests(e.target.value)}
+			/>
+
 			<label htmlFor="occasion">Occasion</label>
-			<select id="occasion">
+			<select
+				id="occasion"
+				value={occasion}
+				onChange={(e) => setOccasion(e.target.value)}
+			>
 				<option>Birthday</option>
 				<option>Anniversary</option>
 			</select>
-			<input type="submit" value="Make Your reservation" />
+
+			<input type="submit" value="Make Reservation" />
 		</form>
 	);
 };
